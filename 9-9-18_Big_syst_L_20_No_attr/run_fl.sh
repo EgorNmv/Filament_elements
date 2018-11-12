9588d9e81fe6765bd59c6c9fb5bf607bfcf15b7a
EgorNmv <43643833+EgorNmv@users.noreply.github.com>
2018-11-12 13:18:15 +0500

    Create run_fl.sh

---
+++ 9-9-18_Big_syst_L_20_No_attr/run_fl.sh
@@ -0,0 +1,75 @@
+#!/bin/bash 
+
+function run {
+   lambda=$1
+   filament_length=$2   
+   pref=$3   
+   np=$4
+   dens=$5
+
+   suffix="L.$lambda.fl.$filament_length.conf.$pref.np.$np.dens.$dens"
+   log="log.$suffix"
+   error="error.$suffix"
+   
+   echo "run $suffix"   
+   srun -t 4200 Espresso320SOS dispersion_XY_checkpoints_new.tcl $lambda $filament_length $pref $np $dens 1>>$log 2>>$error &
+}
+
+np=10752
+filament_length=21
+for pref in 3 ;
+do
+   for lambda in 5 ;
+   do
+      for dens in 0.01 0.05 ;
+      do
+         run $lambda $filament_length $pref $np $dens
+      done
+   done
+done
+
+np=10240
+filament_length=20
+for pref in 1 ;
+do
+   for lambda in 5 ;
+   do
+      for dens in 0.05;
+      do
+         run $lambda $filament_length $pref $np $dens
+      done
+   done
+done
+
+np=9728
+filament_length=19
+for pref in 5 ;
+do
+   for lambda in  5 ;
+   do
+      for dens in 0.05;
+      do
+         run $lambda $filament_length $pref $np $dens
+      done
+   done
+done
+
+np=9728
+filament_length=19
+for pref in 5 ;
+do
+   for lambda in 2 ;
+   do
+      for dens in 0.05;
+      do
+         run $lambda $filament_length $pref $np $dens
+      done
+   done
+done
+
+
+
+
+exit
+
+
