29aaaacf2292f91019be98e031b310f6d6696122
EgorNmv <43643833+EgorNmv@users.noreply.github.com>
2018-11-25 22:52:14 +0500

    Update run_fl.sh

--- 9-9-18_Big_syst_L_20_No_attr/run_fl.sh
+++ 9-9-18_Big_syst_L_20_No_attr/run_fl.sh
@@ -15,17 +15,4 @@ function run {
    srun -t 4200 Espresso320SOS dispersion_XY_checkpoints_new.tcl $lambda $filament_length $pref $np $dens 1>>$log 2>>$error &
 }
 
-np=10752
-filament_length=21
-for pref in 3 ;
-do
-   for lambda in 5 ;
-   do
-      for dens in 0.05 ;
-      do
-         run $lambda $filament_length $pref $np $dens
-      done
-   done
-done
-
 exit
